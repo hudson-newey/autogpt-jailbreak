cf8e987263d8ba043791435ab22e95aba2686bb5
hudson-newey <hudsonnewey@gmail.com>
2023-10-10 21:29:05 +1000

    Fixed script

--- main.pl
+++ main.pl
@@ -3,7 +3,7 @@
 my $autoGptPath = @ARGV[0];
 
 sub escapeWorkspace {
-    my $file = $autoGptPath."/autogps/autogpt/workspace/workspace.py";
+    my $file = $autoGptPath."/autogpts/autogpt/autogpt/workspace/workspace.py";
     print "Removing workspace restrictions...\n";
 
     rename($file, $file . '.bak');
